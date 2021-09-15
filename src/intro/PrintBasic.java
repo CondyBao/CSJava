4e0bb90c193477017cd1aa05d25d947c3b3cc35f
CondyBao <condybao@gmail.com>
2021-09-14 20:08:09 -0400

    Create PrintBasic.java

---
+++ src/intro/PrintBasic.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class PrintBasic {
+    public static void main(String[] args) {
+        System.out.println("What is my name?");
+        System.out.println("My name is Condy Bao.");
+        System.out.println("How old am I?");
+        System.out.println("I am 15 years old.");
+        System.out.println("What is my name?");
+        System.out.println("What is my name?");
+    }
+}
