9f5d40afa8be2350e823e4c6b2694c006b4ff4d8
CondyBao <condybao@gmail.com>
2021-11-12 11:11:56 -0500

    Update

---
+++ src/intro/arrayAlgorithms.java
@@ -0,0 +1,34 @@
+package intro;
+
+import java.util.Arrays;
+
+public class arrayAlgorithms {
+    public static int largeInt(int[] array1) {
+        int maxValue = array1[0], index = 0;
+        for (int i = 1; i < array1.length; i++) {
+            if (maxValue < array1[i]) {
+                maxValue = array1[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static int[] mergeArray(int[] array1, int[] array2) {
+        int[] mergedArray = new int[array1.length + array2.length];
+        for (int i = 0; i < array1.length; i++) {
+            mergedArray[i] = array1[i];
+        }
+        for (int i = 0; i < array2.length; i++) {
+            mergedArray[array1.length + i] = array2[i];
+        }
+        return mergedArray;
+    }
+
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 6, 8};
+        System.out.println(largeInt(array2));
+        System.out.println(Arrays.toString(mergeArray(array1, array2)));
+    }
+}
