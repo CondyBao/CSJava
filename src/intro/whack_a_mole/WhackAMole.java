2d0272b9713813df1cfb84b188b17c1ad866ffa6
CondyBao <condybao@gmail.com>
2021-12-01 13:44:59 -0500

    Update

--- src/intro/whack_a_mole/WhackAMole.java
+++ src/intro/whack_a_mole/WhackAMole.java
@@ -2,16 +2,10 @@ package intro.whack_a_mole;
 
 // Filler-code for Whack a Mole by Mr. Friedman
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.Random;
-
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
 public class WhackAMole extends JPanel{
     Image background = Toolkit.getDefaultToolkit().createImage("background.png");
@@ -19,84 +13,77 @@ public class WhackAMole extends JPanel{
     Image mole = Toolkit.getDefaultToolkit().createImage("mole.png");
 
 	// size of the display area
-    private final int textHeight = 35, holeNum = 10, moleNum = 3, timeChange = 1000, moleHeight = 80, moleWidth = 77;
-    private int timer, score, windowWidth = 1024, windowHeight = 500;
+    private final int textHeight = 35, holeNum = 10, moleNum = 3, timeChange = 3000, moleHeight = 80, moleWidth = 77;
+    private int score, windowWidth = 1024, windowHeight = 500;
     private boolean [] molePlacement = new boolean[holeNum];
     private int[][] map = new int[windowWidth][windowHeight], holePlacement = new int[holeNum][2];
 
-    // more instance variables...?
-
     public void updateHoles() {
-        if (timer % timeChange == 0) {
-            Random rand = new Random();
-            for (int i = 0; i < holeNum; i++) {
-                int random_x = rand.nextInt(windowWidth), random_y = rand.nextInt(windowHeight - textHeight);
-                while (map[random_x][random_y + textHeight] != 0 || map[random_x + moleWidth][random_y + textHeight + moleHeight] != 0){
-                    random_x = rand.nextInt(windowWidth);
-                    random_y = rand.nextInt(windowHeight - textHeight);
-                }
-                holePlacement[i][0] = random_x;
-                holePlacement[i][1] = random_y + textHeight;
-                for (int k = 0; k < moleWidth; k++) {
-                    for (int j = 0; j < moleHeight; j++) {
+        Random rand = new Random();
+        for (int i = 0; i < windowWidth; i++) {
+            for (int j = 0; j < windowHeight; j++) {
+                map[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < holeNum; i++) {
+            molePlacement[i] = false;
+            int random_x, random_y;
+            do {
+                random_x = rand.nextInt(windowWidth - moleWidth);
+                random_y = rand.nextInt(windowHeight - textHeight - moleHeight);
+            } while (map[random_x][random_y + textHeight] != 0 || map[random_x + moleWidth][random_y + textHeight + moleHeight] != 0 || map[random_x][random_y + textHeight + moleHeight] != 0 || map[random_x + moleWidth][random_y + textHeight] != 0);
+            holePlacement[i][0] = random_x;
+            holePlacement[i][1] = random_y + textHeight;
+            for (int k = 0; k < moleWidth; k++) {
+                for (int j = 0; j < moleHeight; j++) {
+                    if ((k + random_x) < windowWidth && (j + random_y + textHeight) < windowHeight) {
                         map[k + random_x][j + random_y + textHeight] = i + 1;
                     }
                 }
             }
-            for (int i = 0; i < moleNum; i++) {
-                int currentMole = rand.nextInt(holeNum);
-                while (molePlacement[currentMole]) {
-                    currentMole = rand.nextInt(holeNum);
-                }
-                molePlacement[currentMole] = true;
+        }
+        for (int i = 0; i < moleNum; i++) {
+            int currentMole = rand.nextInt(holeNum);
+            while (molePlacement[currentMole]) {
+                currentMole = rand.nextInt(holeNum);
             }
+            molePlacement[currentMole] = true;
         }
     }
    
 
     public void draw(Graphics g) {
-        g.drawImage(background, 0, 0, null);
+        g.drawImage(background, 0, 0, windowWidth, windowHeight, null);
         for (int i = 0; i < holeNum; i++) {
             if (molePlacement[i]) {
-                g.drawImage(mole, holePlacement[i][0], holePlacement[i][1], null);
+                g.drawImage(mole, holePlacement[i][0], holePlacement[i][1],null);
             }
             else {
                 g.drawImage(mole_hole, holePlacement[i][0], holePlacement[i][1], null);
             }
         }
-        // your code here
     }
 
 
     // what you want to happen when the mouse is clicked
     public void click(int mouseX, int mouseY) {
-    	
-    	// your code here
+    	int holeID = map[mouseX][mouseY];
+        if (molePlacement[holeID - 1]) {
+            molePlacement[holeID - 1] = false;
+            score++;
+            repaint();
+        }
     }
     
     // reset the game
     public void reset() {
-    	
-    	// your code here
         score = 0;
-        timer = 0;
         updateHoles();
     }
 
-    public void run() {
-        while (true) {
-
-            // redraws the game
-            repaint();
-            timer += 100;
-            //rests for a portion of a second
-        }
-    }
-
-
-    // MAYBE TOUCH THE BELOW CODE? //
 
     public WhackAMole() {
+        updateHoles();
         JFrame window = new JFrame();
         window.setTitle("Whack A Mole");
         window.setSize(windowWidth, windowHeight + textHeight);
@@ -140,7 +127,7 @@ public class WhackAMole extends JPanel{
             @Override
             public void mousePressed(MouseEvent e) {
                 click(e.getX(), e.getY());
-                scoreDisplay.setText("\t\tScore: ");
+                scoreDisplay.setText("\t\tScore: " + score);
                 window.getContentPane().repaint();
             }
 
@@ -171,13 +158,23 @@ public class WhackAMole extends JPanel{
         window.add(container);
         window.setVisible(true);
         canvas.revalidate();
-        run();
+        try {
+            Thread.sleep(70);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         window.getContentPane().repaint();
+        new javax.swing.Timer(timeChange, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                updateHoles();
+                window.getContentPane().repaint();
+            }
+        }).start();
     }
-    
+
 
     public static void main(String[] args) {
         new WhackAMole();
     }
-
 }
