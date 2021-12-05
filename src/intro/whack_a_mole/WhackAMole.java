d9860a1a548847e5e16ea32d297a3f780efb890d
CondyBao <condybao@gmail.com>
2021-12-05 16:26:32 -0500

    Update

--- src/intro/whack_a_mole/WhackAMole.java
+++ src/intro/whack_a_mole/WhackAMole.java
@@ -8,58 +8,88 @@ import java.awt.event.*;
 import java.util.Random;
 
 public class WhackAMole extends JPanel{
+    //importing images
     Image background = Toolkit.getDefaultToolkit().createImage("background.png");
     Image mole_hole = Toolkit.getDefaultToolkit().createImage("mole_hole.png");
     Image mole = Toolkit.getDefaultToolkit().createImage("mole.png");
+    Image bonus = Toolkit.getDefaultToolkit().createImage("bonus.png");
+    Image bomb = Toolkit.getDefaultToolkit().createImage("bomb.png");
 
-	// size of the display area
-    private final int textHeight = 35, holeNum = 10, moleNum = 3, timeChange = 3000, moleHeight = 80, moleWidth = 77;
+	// creating variables
+    private final int textHeight = 35, holeNum = 10, moleNum = 3, timeChange = 5000, moleHeight = 80, moleWidth = 77;
     private int score, windowWidth = 1024, windowHeight = 500;
-    private boolean [] molePlacement = new boolean[holeNum];
-    private int[][] map = new int[windowWidth][windowHeight], holePlacement = new int[holeNum][2];
+    private final boolean [] molePlacement = new boolean[holeNum], bombPlacement = new boolean[holeNum], bonusPlacement = new boolean[holeNum];
+    private final int[][] map = new int[windowWidth][windowHeight], holePlacement = new int[holeNum][2];
 
     public void updateHoles() {
-        Random rand = new Random();
-        for (int i = 0; i < windowWidth; i++) {
+        Random rand = new Random(); //generating random
+        for (int i = 0; i < windowWidth; i++) { //clear map
             for (int j = 0; j < windowHeight; j++) {
                 map[i][j] = 0;
             }
         }
-        for (int i = 0; i < holeNum; i++) {
+        for (int i = 0; i < holeNum; i++) { //find holes to place
+            //reset all the boolean arrays
             molePlacement[i] = false;
-            int random_x, random_y;
+            bonusPlacement[i] = false;
+            bombPlacement[i] = false;
+            int random_x, random_y; //two randomly generated integers for the next hole's coordinates
             do {
+                //generating values
                 random_x = rand.nextInt(windowWidth - moleWidth);
                 random_y = rand.nextInt(windowHeight - textHeight - moleHeight);
-            } while (map[random_x][random_y + textHeight] != 0 || map[random_x + moleWidth][random_y + textHeight + moleHeight] != 0 || map[random_x][random_y + textHeight + moleHeight] != 0 || map[random_x + moleWidth][random_y + textHeight] != 0);
-            holePlacement[i][0] = random_x;
-            holePlacement[i][1] = random_y + textHeight;
-            for (int k = 0; k < moleWidth; k++) {
+            } while (map[random_x][random_y + textHeight] != 0 || map[random_x + moleWidth][random_y + textHeight + moleHeight] != 0 || map[random_x][random_y + textHeight + moleHeight] != 0 || map[random_x + moleWidth][random_y + textHeight] != 0); //assuming each hole takes the area of a rectangle, making sure the area is not taken by judging the four vertices
+            holePlacement[i][0] = random_x; //recording the successful values
+            holePlacement[i][1] = random_y + textHeight; //adding the top bar's texts' height too
+            for (int k = 0; k < moleWidth; k++) { //updating the map by coloring each rectangle to its id(i + 1)
                 for (int j = 0; j < moleHeight; j++) {
-                    if ((k + random_x) < windowWidth && (j + random_y + textHeight) < windowHeight) {
-                        map[k + random_x][j + random_y + textHeight] = i + 1;
+                    if ((k + random_x) < windowWidth && (j + random_y + textHeight) < windowHeight) { //making sure the selected value is not out of bounds
+                        map[k + random_x][j + random_y + textHeight] = i + 1; //setting to i + 1 to avoid 0(which is the original value)
                     }
                 }
             }
         }
+        int random_num = rand.nextInt(4); //generate a random number to determine if a bonus or a bomb will be placed
+        if (random_num == 0 || random_num == 1) { //1/2 chance of getting a bomb each round
+            int currentBomb = 0;
+            do {
+                currentBomb = rand.nextInt(holeNum);
+            } while (bombPlacement[currentBomb]); //selecting an empty hole
+            bombPlacement[currentBomb] = true; //setting the hole to a bomb
+        }
+        if (random_num == 2) {
+            int currentBonus = 0;
+            do {
+                currentBonus = rand.nextInt(holeNum);
+            } while (bombPlacement[currentBonus] || bonusPlacement[currentBonus]); //selecting an empty hole
+            bonusPlacement[currentBonus] = true; //setting the hole to a gift box
+        }
         for (int i = 0; i < moleNum; i++) {
-            int currentMole = rand.nextInt(holeNum);
-            while (molePlacement[currentMole]) {
+            int currentMole = 0;
+            do {
                 currentMole = rand.nextInt(holeNum);
-            }
-            molePlacement[currentMole] = true;
+            } while (molePlacement[currentMole] || bombPlacement[currentMole] || bonusPlacement[currentMole]); // selecting an empty hole
+            molePlacement[currentMole] = true; //setting the hole to a mole
         }
     }
    
 
     public void draw(Graphics g) {
-        g.drawImage(background, 0, 0, windowWidth, windowHeight, null);
+        g.drawImage(background, 0, 0, windowWidth, windowHeight, null); // draw background
         for (int i = 0; i < holeNum; i++) {
-            if (molePlacement[i]) {
-                g.drawImage(mole, holePlacement[i][0], holePlacement[i][1],null);
+            if (molePlacement[i]) { // if the hole is a mole
+                g.drawImage(mole, holePlacement[i][0], holePlacement[i][1],null); // draw mole
             }
             else {
-                g.drawImage(mole_hole, holePlacement[i][0], holePlacement[i][1], null);
+                if (bombPlacement[i]) { // if the hole is a bomb
+                    g.drawImage(bomb, holePlacement[i][0], holePlacement[i][1], null); // draw bomb
+                }
+                else {
+                    if (bonusPlacement[i]) { // if the hole is a gift box
+                        g.drawImage(bonus, holePlacement[i][0], holePlacement[i][1], null); // draw gift box
+                    }
+                    else g.drawImage(mole_hole, holePlacement[i][0], holePlacement[i][1],null); // if the hole is just a hole, draw nothing
+                }
             }
         }
     }
@@ -67,18 +97,26 @@ public class WhackAMole extends JPanel{
 
     // what you want to happen when the mouse is clicked
     public void click(int mouseX, int mouseY) {
-    	int holeID = map[mouseX][mouseY];
-        if (molePlacement[holeID - 1]) {
-            molePlacement[holeID - 1] = false;
-            score++;
-            repaint();
+    	int holeID = map[mouseX][mouseY]; // getting the hole's id using the map array initiated earlier
+        if (holeID == 0) return; // if there's nothing, return
+        if (molePlacement[holeID - 1]) { // if it is a mole
+            molePlacement[holeID - 1] = false; // make the mole disappear
+            score++; // add to the score
+        }
+        if (bombPlacement[holeID - 1]) {
+            bombPlacement[holeID - 1] = false; // make the bomb disappear
+            score = 0; // clear the score
+        }
+        if (bonusPlacement[holeID - 1]) {
+            bonusPlacement[holeID - 1] = false; // make the gift box disappear
+            score += 5; // add to the score
         }
     }
     
     // reset the game
     public void reset() {
-        score = 0;
-        updateHoles();
+        score = 0; // clear the score
+        updateHoles(); // regenerate holes
     }
 
 
@@ -127,7 +165,7 @@ public class WhackAMole extends JPanel{
             @Override
             public void mousePressed(MouseEvent e) {
                 click(e.getX(), e.getY());
-                scoreDisplay.setText("\t\tScore: " + score);
+                scoreDisplay.setText("\t\tScore: " + score + "       " + "Don't Click on Bombs and Click on Gifts");
                 window.getContentPane().repaint();
             }
 
@@ -156,14 +194,14 @@ public class WhackAMole extends JPanel{
         container.add(topPanel);
         container.add(canvas);
         window.add(container);
-        window.setVisible(true);
         canvas.revalidate();
         try {
-            Thread.sleep(70);
+            Thread.sleep(50);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         window.getContentPane().repaint();
+        window.setVisible(true);
         new javax.swing.Timer(timeChange, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
