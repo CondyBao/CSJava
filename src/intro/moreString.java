043d1574e7088e965d6466583922503d17b96ee0
CondyBao <condybao@gmail.com>
2021-12-08 12:55:20 -0500

    Update

---
+++ src/intro/moreString.java
@@ -0,0 +1,44 @@
+package intro;
+
+public class moreString {
+    public static void end2(String a, String b) {
+        System.out.println(a.endsWith(b.substring(b.length() - 2)));
+    }
+
+    public static void palindrome(String a) {
+        boolean isPalindrome = true;
+        for (int i = 0; i <= a.length() / 2; i++) isPalindrome = isPalindrome && a.charAt(i) == a.charAt(a.length() - i - 1);
+        System.out.println(isPalindrome);
+    }
+
+    public static void takeOut(String a) {
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != 'e') System.out.print(a.charAt(i));
+        }
+        System.out.println();
+    }
+
+    public static void longBlock (String a) {
+        int length = 1;
+        for (int i = 0; i < a.length(); i++) {
+            int newLength = 1;
+            while (newLength + i < a.length() && a.charAt(newLength + i) == a.charAt(i)) newLength++;
+            length = Math.max(length, newLength);
+        }
+        System.out.println(length);
+    }
+
+    public static void words (String a) {
+        int cnt = 1;
+        for (int i = 0; i < a.length(); i++) if (a.charAt(i) == ' ') cnt++;
+        System.out.println(cnt);
+    }
+
+    public static void main(String[] args) {
+        end2("happy", "puppy");
+        palindrome("helloolleh");
+        takeOut("Hello");
+        longBlock("Helllooooo");
+        words("Hello World!");
+    }
+}
